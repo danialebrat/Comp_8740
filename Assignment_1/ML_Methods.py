3b2e2f799fade34058d5f7b2eff4f6ca84620f09
danialebrat <danial.ebrat@gmail.com>
2022-09-27 00:23:40 -0400

    Datasets object: read datasets ML-Methods: preprocessing and method's implementations

--- Assignment_1/ML_Methods.py
+++ Assignment_1/ML_Methods.py
@@ -10,60 +10,62 @@ class ML_Methods:
         self.name = name
         self.dataset = dataset
 
-        def trainValSplit_Kfold(self, dataset, num_repeat=1, num_split=10, random_state=None):
-            """
-            Create dataset and divide dataset to train and test set with number of folding which user has desired.
-            Args:
-            ---
-                `num_repeat` (`int`, optional): How many times this folding should be repeated. Defaults to 1.
-                `num_split` (`int`, optional): Number of folding/ spliting dataset. Defaults to 10.
-                `random_state` (`random_state`, optional): The state of Randomization. Defaults to None.
+    def trainValSplit_Kfold(self, dataset, num_repeat=1, num_split=10, random_state=None):
+        """
+        Create dataset and divide dataset to train and test set with number of folding which user has desired.
+        Args:
+        ---
+            `num_repeat` (`int`, optional): How many times this folding should be repeated. Defaults to 1.
+            `num_split` (`int`, optional): Number of folding/ spliting dataset. Defaults to 10.
+            `random_state` (`random_state`, optional): The state of Randomization. Defaults to None.
 
-            Return: 4 list of datasets which are splited and folded.
+        Return: 4 list of datasets which are splited and folded.
 
-            Example for return:
-                out = ds.trainValCreation()
-                '''
-                    out[0][0] --> the first train-set
-                    ...
-                    out[0][9] --> the tenth train-set
+        Example for return:
+            out = ds.trainValCreation()
+            '''
+                out[0][0] --> the first train-set
+                ...
+                out[0][9] --> the tenth train-set
 
-                    out[1][0] --> the first test_set
-                    ...
-                    out[1][9] --> the tenth test_set
+                out[1][0] --> the first test_set
+                ...
+                out[1][9] --> the tenth test_set
 
-                    out[2][0] --> the first train_targets
-                    ...
-                    out[2][9] --> the tenth train_targets
+                out[2][0] --> the first train_targets
+                ...
+                out[2][9] --> the tenth train_targets
 
-                    out[3][0] --> the first test_targets
-                    ...
-                    out[3][9] --> the tenth test_targets
-                '''
-            """
+                out[3][0] --> the first test_targets
+                ...
+                out[3][9] --> the tenth test_targets
+            '''
+        """
+        raw_X = dataset.iloc[:, :-1].values
+        Y = dataset.iloc[:, -1].values
 
-            X = dataset[:, :-1]
-            Y = dataset[:, -1]
+        scaler = MinMaxScaler()
+        X = scaler.fit_transform(raw_X)
 
-            x_train = []
-            x_test = []
-            y_train = []
-            y_test = []
+        x_train = []
+        x_test = []
+        y_train = []
+        y_test = []
 
-            kf = RepeatedKFold(n_splits=num_split, n_repeats=num_repeat, random_state=random_state)
-            for train_index, test_index in kf.split(X):
-                # print("Train:", train_index, "Validation:",test_index)
-                x_train.append(X[train_index])
-                x_test.append(X[test_index])
-                y_train.append(Y[train_index])
-                y_test.append(Y[test_index])
+        kf = RepeatedKFold(n_splits=num_split, n_repeats=num_repeat, random_state=random_state)
+        for train_index, test_index in kf.split(X):
+            # print("Train:", train_index, "Validation:",test_index)
+            x_train.append(X[train_index])
+            x_test.append(X[test_index])
+            y_train.append(Y[train_index])
+            y_test.append(Y[test_index])
 
-            print("X_train shape: ".format(np.array(x_train).shape))
-            print("X_test shape: ".format(np.array(x_test).shape))
-            print("Y_test shape: ".format(np.array(y_train).shape))
-            print("Y_test shape: ".format(np.array(y_test).shape))
+        print("X_train shape: ".format(np.array(x_train).shape))
+        print("X_test shape: ".format(np.array(x_test).shape))
+        print("Y_test shape: ".format(np.array(y_train).shape))
+        print("Y_test shape: ".format(np.array(y_test).shape))
 
-            return x_train, x_test, y_train, y_test
+        return x_train, x_test, y_train, y_test
 
     def preprocess(self, df):
         X = df.iloc[:, :-1].values
--- Assignment_1/Main.py
+++ Assignment_1/Main.py
@@ -2,7 +2,7 @@ from Assignment_1.Datasets import Datasets
 from Assignment_1.ML_Methods import ML_Methods
 
 # Path to the folder of Datasets
-FOLDER_PATH = "C:/Users/User/PycharmProjects/Comp_8740/Assignment_1/Datasets"
+FOLDER_PATH = "C:/Users/User/PycharmProjects/Comp_8740/Assignment_1/Assignment_1/Datasets"
 
 
 if __name__ == "__main__":
@@ -14,8 +14,8 @@ if __name__ == "__main__":
 
     for name,data in zip(Data.Name_List, Data.Data_List):
 
+        # creating and ML_method object for each dataset
         method = ML_Methods(name=name, dataset=data)
-        x, y = method.preprocess(data)
 
         # spliting the dataset
         x_train, x_test, y_train, y_test = method.trainValSplit_Kfold(data)
