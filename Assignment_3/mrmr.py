0f43d5f1bf5078c2d540aca649b693bb2ea3fb83
Mustafa Muhammadi <mustafa.mohammadi@ut.ac.ir>
2022-11-08 09:17:47 -0500

    .

--- Assignment_3/mrmr.py
+++ Assignment_3/mrmr.py
@@ -23,8 +23,8 @@ from sklearn.metrics import confusion_matrix, accuracy_score
 dataset = pd.read_csv(os.getcwd()+'/Datasets/Breastcancer.csv')
 dataset.replace([np.inf, -np.inf], np.nan, inplace=True)
 
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1].values
+X = dataset.iloc[:, :-1]
+y_ = dataset.iloc[:, -1]
 
 ################################################################
 # %%
@@ -32,17 +32,9 @@ y = dataset.iloc[:, -1].values
 
 
 le = LabelEncoder()
-y = le.fit_transform(y)
+y = le.fit_transform(y_)
 
 print('number of samples and labels: {}'.format(y.shape))
-
-################################################################
-#%%
-#Normalize samples' features
-
-
-min_max_scaler = preprocessing.MinMaxScaler()
-X_train_minmax = min_max_scaler.fit_transform(X)
 
 ################################################################
 #%%
@@ -52,27 +44,36 @@ y = dataset['Class']
 ###############################################################
 # DO NOT RUN THIS SECTION UNTIL YOU NEED TO RECACULATE THE RMRM FEATURE SELECTION,
 # IT TAKES LONG TIMES TO RUN
+
+# #%% 
+# number_of_features_as_K = [500, 1000, 1500]
+# mrmr = {}
+# for i, k in enumerate(number_of_features_as_K):
+#     mrmr[k] = mRMR_Feature_Selector.mrmr_classif(X = X, y=y, K=k)
+#     with open('mrmr_features_{}'.format(str(k)), 'wb') as fp:  # pickling
+#         pk.dump(mrmr[k], fp)
+#     fp.close()
+# # print('List of features selected by mRMR :', mrmr)
+
+# ################################################################
+# #%% 
+# # saving mrmr1 features as a file for loading later
+# number_of_features_as_K = [500, 1000, 1500]
+# for _, k in enumerate(number_of_features_as_K):
+#     with open('mrmr_features_{}'.format(str(k)), 'wb') as fp:  # pickling
+#         pk.dump(mrmr[k], fp)
+#     fp.close()
+
+################################################################
 #%% 
+# Loading mrmr1 featers from the file
 number_of_features_as_K = [500, 1000, 1500]
 mrmr = {}
-for i, k in enumerate(number_of_features_as_K):
-    mrmr[str(k)] = mRMR_Feature_Selector.mrmr_classif(X = X, y=y, K=k)
-# print('List of features selected by mRMR :', mrmr)
-
-################################################################
-#%% 
-# saving mrmr1 features as a file for loading later
-number_of_features_as_K = [500, 1000, 1500]
-for _, k in enumerate(number_of_features_as_K):
-    with open('mrmr_features_{}'.format(str(k)), 'wb') as fp:  # pickling
-        pk.dump(mrmr[str(k)], fp)
-
-################################################################
-#%% 
-# loading mrmr1 featers from the file
 for _, k in enumerate(number_of_features_as_K):
     with open('mrmr_features_{}'.format(str(k)), 'rb') as fp:  # unpickling
-        pk.dump(mrmr[str(k)], fp)
+        mrmr[k] = pk.load(fp)
+    fp.close()
+
 
 # with open('mrmr_features', 'rb') as fp:  # unpickling
 #     mrmr1 = pk.load(fp)
@@ -84,15 +85,18 @@ X_train = {}
 X_test = {}
 y_train = {}
 y_test = {}
+
+# Normalize samples' features
+min_max_scaler = preprocessing.MinMaxScaler()
+X_train_minmax = min_max_scaler.fit_transform(X)
+
 for _, k in enumerate(number_of_features_as_K):
-    X_mrmr[str(k)] = X[mrmr[str(k)]]
-    X_train[str(k)], X_test[str(k)], y_train[str(k)], y_test[str(k)] = train_test_split(X_mrmr[mrmr[str(k)]], 
-                                                                                        y[mrmr[str(k)]], 
-                                                                                        test_size = 0.2, 
-                                                                                        random_state = 42)
+    X_mrmr[k] = X[mrmr[k]]
+    # y = y_[mrmr[k]]
 
+    X_train[k], X_test[k], y_train[k], y_test[k] = train_test_split(X_mrmr[k], y, test_size = 0.2, random_state = 42)
 
-print('type of targets', y_test[str(k)].unique())
+    print('number of samples in testset for k of {} is {}'.format(k, len(y_train[k])))
 
 
 ################################################################
@@ -100,19 +104,19 @@ print('type of targets', y_test[str(k)].unique())
 # model selection
 clf = {}
 for _, k in enumerate(number_of_features_as_K):
-    clf = SVC(kernel = 'rbf', random_state = 42, decision_function_shape='ovr')
-    clf.fit(X_train[str(k)], y_train[str(k)])
+    clf[k] = SVC(kernel = 'rbf', random_state = 42, decision_function_shape='ovr')
+    clf[k].fit(X_train[k], y_train[k])
 
 ################################################################
 #%%
 y_pred = {}
 cm = {}
 for _, k in enumerate(number_of_features_as_K):
-    y_pred[str(k)] = clf.predict(X_test[str(k)])
+    y_pred[k] = clf[k].predict(X_test[k])
     # Confusion matrix
-    cm[str(k)] = confusion_matrix(y_test[str(k)], y_pred[str(k)])
+    cm[k] = confusion_matrix(y_test[k], y_pred[k])
     
-    print('confusion matrix for {} features =\n {}'.format(k, cm[str(k)]))
+    print('confusion matrix for {} features =\n {}'.format(k, cm[k]))
 
 
 ################################################################
@@ -120,55 +124,60 @@ for _, k in enumerate(number_of_features_as_K):
 # 10-folded cross validation and metric calculation
 accuracies = {}
 for _, k in enumerate(number_of_features_as_K):
-    accuracies[str(k)] = cross_val_score(estimator = clf, X = X_train[str(k)], y = y_train[str(k)], cv = 10)
-    print("Accuracy for {} features : {:.2f} %".format(k, accuracies[str(k)].mean()*100))
-    print("Standard Deviation for {} features : {:.2f} %".format(k, accuracies[str(k)].std()*100))
+    accuracies[k] = cross_val_score(estimator = clf[k], X = X_train[k], y = y_train[k], cv = 10)
+    print("Accuracy for {} features : {:.2f} %".format(k, accuracies[k].mean()*100))
+    print("Standard Deviation for {} features : {:.2f} %".format(k, accuracies[k].std()*100))
 
 
 ################################################################
 #%%
-FP = cm.sum(axis=0) - np.diag(cm)  
-FN = cm.sum(axis=1) - np.diag(cm)
-TP = np.diag(cm)
-TN = cm.sum() - (FP + FN + TP)
+def other_measures(cm, k):
+    print(type(cm))
+    FP = cm.sum(axis=0) - np.diag(cm)  
+    FN = cm.sum(axis=1) - np.diag(cm)
+    TP = np.diag(cm)
+    TN = cm.sum() - (FP + FN + TP)
 
-print(FP)
-print(FN)
-print(TP)
-print(TN)
-
-
-# Sensitivity, hit rate, recall, or true positive rate
-TPR = TP/(TP+FN)
-# Specificity or true negative rate
-TNR = TN/(TN+FP) 
-# Precision or positive predictive value
-PPV = TP/(TP+FP)
-# Negative predictive value
-NPV = TN/(TN+FN)
-# Fall out or false positive rate
-FPR = FP/(FP+TN)
-# False negative rate
-FNR = FN/(TP+FN)
-# False discovery rate
-FDR = FP/(TP+FP)
-
-# Overall accuracy
-ACC = (TP+TN)/(TP+FP+FN+TN)
-
-print("PPV:{}\nNPV:{}\nSensitivity:{}\nSpecificity:{}".format(PPV, NPV, TPR, TNR))
-print('\n')
-print("Accuracy:",ACC)
+    print(FP)
+    print(FN)
+    print(TP)
+    print(TN)
 
 
-avg_PPV  = np.average(PPV, axis=None, weights=None, returned=False)
-avg_NPV  = np.average(NPV, axis=None, weights=None, returned=False)
-avg_TPR  = np.average(TPR, axis=None, weights=None, returned=False)
-avg_TNR  = np.average(TNR, axis=None, weights=None, returned=False)
-avg_ACC  = np.average(ACC, axis=None, weights=None, returned=False)
-print("PPV:{:.2f}\nNPV:{:.2f}\nSensitivity:{:.2f}\nSpecificity:{:.2f}\nAccuracy:{:.2f}".format(avg_PPV, avg_NPV, avg_TPR, avg_TNR, avg_ACC))
+    # Sensitivity, hit rate, recall, or true positive rate
+    TPR = TP/(TP+FN)
+    # Specificity or true negative rate
+    TNR = TN/(TN+FP) 
+    # Precision or positive predictive value
+    PPV = TP/(TP+FP)
+    # Negative predictive value
+    NPV = TN/(TN+FN)
+    # Fall out or false positive rate
+    FPR = FP/(FP+TN)
+    # False negative rate
+    FNR = FN/(TP+FN)
+    # False discovery rate
+    FDR = FP/(TP+FP)
 
+    # Overall accuracy
+    ACC = (TP+TN)/(TP+FP+FN+TN)
 
+    print('\n\n',"#"*20,' Measurments for {} features '.format(k)," ","#"*20,'\n')
+
+    print("PPV:{}\nNPV:{}\nSensitivity:{}\nSpecificity:{}".format(PPV, NPV, TPR, TNR))
+    print('\n')
+    print("Accuracy:",ACC)
+
+    avg_PPV  = np.average(PPV, axis=None, weights=None, returned=False)
+    avg_NPV  = np.average(NPV, axis=None, weights=None, returned=False)
+    avg_TPR  = np.average(TPR, axis=None, weights=None, returned=False)
+    avg_TNR  = np.average(TNR, axis=None, weights=None, returned=False)
+    avg_ACC  = np.average(ACC, axis=None, weights=None, returned=False)
+    print("PPV:{:.2f}\nNPV:{:.2f}\nSensitivity:{:.2f}\nSpecificity:{:.2f}\nAccuracy:{:.2f}".format(avg_PPV, avg_NPV, avg_TPR, avg_TNR, avg_ACC))
+    print('\n\n\n')
+
+for _, k in enumerate(number_of_features_as_K):
+    other_measures(cm[k], k)
 ################################################################
 #%%
 #
