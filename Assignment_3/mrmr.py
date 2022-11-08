e2b564b522d69a37b841fdae1386caae6e5f0869
Mustafa Muhammadi <mustafa.mohammadi@ut.ac.ir>
2022-11-07 22:02:06 -0500

    .

--- Assignment_3/mrmr.py
+++ Assignment_3/mrmr.py
@@ -1,84 +1,84 @@
 
 # this code is not running properly
 #%%
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-import os
+import My_RMRM
 
+import os
+import numpy as np
+import pandas as pd
+
+from sklearn import preprocessing
+from sklearn.preprocessing import LabelEncoder
+from sklearn.svm import SVC
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2, mutual_info_classif
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+import matplotlib.pyplot as plt
+################################################################
 #%%
+#Read the dataset
 dataset = pd.read_csv(os.getcwd()+'/Datasets/Breastcancer.csv')
 dataset.replace([np.inf, -np.inf], np.nan, inplace=True)
 
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
-
+################################################################
 # %%
-from sklearn.preprocessing import LabelEncoder
+# Encode labels
+
+
 le = LabelEncoder()
 y = le.fit_transform(y)
 
-print(y)
-y.shape
+print('number of samples and labels: {}'.format(y.shape))
+
+################################################################
 #%%
-from sklearn import preprocessing
+#Normalize samples' features
+
+
 min_max_scaler = preprocessing.MinMaxScaler()
 X_train_minmax = min_max_scaler.fit_transform(X)
-X_train_minmax.max()
 
+################################################################
 #%%
-from sklearn.feature_selection import chi2, mutual_info_classif
-chi_scores = chi2(X_train_minmax,y)
-information_gain = mutual_info_classif(X_train_minmax, y)
-print(chi_scores)
-print(information_gain)
 
-
-print(chi_scores[0].max())
-print(information_gain.max())
+number_of_features_as_K = 500
 
 
 X = dataset.drop('Class',axis=1)
 y = dataset['Class']
-p_values = pd.Series(chi_scores[1],index = X.columns)
-p_values.sort_values(ascending = False , inplace = True)
-p_values_IG = pd.Series(information_gain, index = X.columns)
-p_values_IG.sort_values(ascending = False, inplace= True)
 
+################################################################
+#%%
 
-p_values.plot.bar()
-p_values_IG.plot.bar()
-
-
-from sklearn.feature_selection import SelectKBest
-chi2_selector = SelectKBest(chi2, k=500)
-X_kbest = chi2_selector.fit_transform(X_train_minmax, y)
-IG_selector = SelectKBest(mutual_info_classif, k=500)
-X_kbest_IG = IG_selector.fit_transform(X_train_minmax, y)
-print(X_kbest)
-print(X_kbest_IG)
-
-
+mrmr1 = My_RMRM.mrmr_classif(X = X, y=y, K=500)
 print('Original number of features:', X.shape)
-print('Reduced number of features:', X_kbest.shape)
+# print('Reduced number of features:', X_kbest.shape)
 
-from sklearn.model_selection import train_test_split
+################################################################
+#%%
+
 X_train, X_test, y_train, y_test = train_test_split(X_kbest, y, test_size = 0.2, random_state = 42)
 X_train_IG, X_test_IG, y_train_IG, y_test_IG = train_test_split(X_kbest_IG, y, test_size = 0.2, random_state = 42)
 print(y_test.unique())
 print(y_test_IG.unique())
 
 # model selection
-from sklearn.svm import SVC
+
 classifier = SVC(kernel = 'rbf', random_state = 42, decision_function_shape='ovr')
 classifier.fit(X_train, y_train)
 classifier_IG = SVC(kernel = 'rbf', random_state = 42, decision_function_shape='ovr')
 classifier_IG.fit(X_train_IG, y_train_IG)
 
+################################################################
 #%%
 # Confusion matrix
-from sklearn.metrics import confusion_matrix, accuracy_score
+
 y_pred = classifier.predict(X_test)
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
@@ -87,10 +87,10 @@ y_pred_IG = classifier.predict(X_test_IG)
 cm_IG = confusion_matrix(y_test_IG, y_pred_IG)
 print(cm_IG)
 
-
+################################################################
 #%%
 # 10-folded cross validation and metric calculation
-from sklearn.model_selection import cross_val_score
+
 accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
 print("Accuracy: {:.2f} %".format(accuracies.mean()*100))
 print("Standard Deviation: {:.2f} %".format(accuracies.std()*100))
@@ -99,6 +99,8 @@ accuracie_IG = cross_val_score(estimator = classifier, X = X_train_IG, y = y_tra
 print("IG Accuracy: {:.2f} %".format(accuracie_IG.mean()*100))
 print("IG Standard Deviation: {:.2f} %".format(accuracie_IG.std()*100))
 
+################################################################
+#%%
 FP = cm.sum(axis=0) - np.diag(cm)  
 FN = cm.sum(axis=1) - np.diag(cm)
 TP = np.diag(cm)
@@ -178,6 +180,8 @@ avg_TPR_IG  = np.average(TPR_IG, axis=None, weights=None, returned=False)
 avg_TNR_IG  = np.average(TNR_IG, axis=None, weights=None, returned=False)
 avg_ACC_IG  = np.average(ACC_IG, axis=None, weights=None, returned=False)
 print("\nPPV:{:.2f}\nNPV:{:.2f}\nSensitivity:{:.2f}\nSpecificity:{:.2f}\nAccuracy:{:.2f}".format(avg_PPV_IG, avg_NPV_IG, avg_TPR_IG, avg_TNR_IG, avg_ACC_IG))
+
+################################################################
 #%%
 #
 
