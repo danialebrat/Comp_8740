0b78f6b642459f394228e9efb1f3856bc1964d2c
Mustafa Muhammadi <mustafa.mohammadi@ut.ac.ir>
2022-11-08 16:38:14 -0500

    result of MRMR with three classifier

--- Assignment_3/mrmr.py
+++ Assignment_3/mrmr.py
@@ -11,6 +11,8 @@ import pickle as pk # for saving the list
 from sklearn import preprocessing
 from sklearn.preprocessing import LabelEncoder
 from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier 
+from sklearn.tree import DecisionTreeClassifier
 # from sklearn.feature_selection import SelectKBest
 # from sklearn.feature_selection import chi2, mutual_info_classif
 from sklearn.model_selection import train_test_split
@@ -42,32 +44,32 @@ X = dataset.drop('Class',axis=1)
 y = dataset['Class']
 
 ###############################################################
+#%% 
 # DO NOT RUN THIS SECTION UNTIL YOU NEED TO RECACULATE THE RMRM FEATURE SELECTION,
 # IT TAKES LONG TIMES TO RUN
 
-# #%% 
-# number_of_features_as_K = [500, 1000, 1500]
-# mrmr = {}
-# for i, k in enumerate(number_of_features_as_K):
-#     mrmr[k] = mRMR_Feature_Selector.mrmr_classif(X = X, y=y, K=k)
-#     with open('mrmr_features_{}'.format(str(k)), 'wb') as fp:  # pickling
-#         pk.dump(mrmr[k], fp)
-#     fp.close()
-# # print('List of features selected by mRMR :', mrmr)
+number_of_features_as_K = [50, 80, 100, 150, 200]
+mrmr = {}
+for i, k in enumerate(number_of_features_as_K):
+    mrmr[k] = mRMR_Feature_Selector.mrmr_classif(X = X, y=y, K=k)
+    with open('mrmr_features_{}'.format(str(k)), 'wb') as fp:  # pickling
+        pk.dump(mrmr[k], fp)
+    fp.close()
+# print('List of features selected by mRMR :', mrmr)
 
-# ################################################################
-# #%% 
-# # saving mrmr1 features as a file for loading later
-# number_of_features_as_K = [500, 1000, 1500]
-# for _, k in enumerate(number_of_features_as_K):
-#     with open('mrmr_features_{}'.format(str(k)), 'wb') as fp:  # pickling
-#         pk.dump(mrmr[k], fp)
-#     fp.close()
+################################################################
+#%% 
+# saving mrmr1 features as a file for loading later
+number_of_features_as_K = [50, 80, 100, 150, 200, 500, 1000, 1500]
+for _, k in enumerate(number_of_features_as_K):
+    with open('mrmr_features_{}'.format(str(k)), 'wb') as fp:  # pickling
+        pk.dump(mrmr[k], fp)
+    fp.close()
 
 ################################################################
 #%% 
 # Loading mrmr1 featers from the file
-number_of_features_as_K = [500, 1000, 1500]
+number_of_features_as_K = [50, 80, 100, 150, 200, 500, 1000, 1500]
 mrmr = {}
 for _, k in enumerate(number_of_features_as_K):
     with open('mrmr_features_{}'.format(str(k)), 'rb') as fp:  # unpickling
@@ -103,41 +105,67 @@ for _, k in enumerate(number_of_features_as_K):
 #%%
 # model selection
 clf = {}
+RF = {}
+DT = {}
 for _, k in enumerate(number_of_features_as_K):
     clf[k] = SVC(kernel = 'rbf', random_state = 42, decision_function_shape='ovr')
     clf[k].fit(X_train[k], y_train[k])
+    RF[k] = RandomForestClassifier(criterion='entropy')
+    RF[k].fit(X_train[k], y_train[k])
+    DT[k] = RandomForestClassifier()
+    DT[k].fit(X_train[k], y_train[k])
 
 ################################################################
 #%%
 y_pred = {}
+y_pred_DT = {}
+y_pred_RF = {}
+
 cm = {}
+cm_DT = {}
+cm_RF = {}
+
 for _, k in enumerate(number_of_features_as_K):
     y_pred[k] = clf[k].predict(X_test[k])
+    y_pred_RF[k] = RF[k].predict(X_test[k])
+    y_pred_DT[k] = RF[k].predict(X_test[k])
     # Confusion matrix
     cm[k] = confusion_matrix(y_test[k], y_pred[k])
+    cm_RF[k] = confusion_matrix(y_test[k], y_pred[k])
+    cm_DT[k] = confusion_matrix(y_test[k], y_pred[k])
     
-    print('confusion matrix for {} features =\n {}'.format(k, cm[k]))
+    print('SVM_confusion matrix for {} features =\n {}'.format(k, cm[k]))
+    print('RF_confusion matrix for {} features =\n {}'.format(k, cm_RF[k]))
+    print('DT_confusion matrix for {} features =\n {}'.format(k, cm_DT[k]))
 
 
 ################################################################
 #%%
 # 10-folded cross validation and metric calculation
 accuracies = {}
+accuracies_RF = {}
+accuracies_DT = {}
 for _, k in enumerate(number_of_features_as_K):
     accuracies[k] = cross_val_score(estimator = clf[k], X = X_train[k], y = y_train[k], cv = 10)
-    print("Accuracy for {} features : {:.2f} %".format(k, accuracies[k].mean()*100))
-    print("Standard Deviation for {} features : {:.2f} %".format(k, accuracies[k].std()*100))
+    accuracies_RF[k] = cross_val_score(estimator = RF[k], X = X_train[k], y = y_train[k], cv = 10)
+    accuracies_DT[k] = cross_val_score(estimator = RF[k], X = X_train[k], y = y_train[k], cv = 10)
+    print("SVM_Accuracy using {} features : {:.2f} %".format(k, accuracies[k].mean()*100))
+    print("RF_Accuracy using {} features : {:.2f} %".format(k, accuracies_RF[k].mean()*100))
+    print("DT_Accuracy using {} features : {:.2f} %".format(k, accuracies_DT[k].mean()*100))
+
+    # print("Standard Deviation for {} features : {:.2f} %".format(k, accuracies[k].std()*100))
 
 
 ################################################################
 #%%
-def other_measures(cm, k):
+def other_measures(cm, k, classifier_ = 'SVM'):
     print(type(cm))
     FP = cm.sum(axis=0) - np.diag(cm)  
     FN = cm.sum(axis=1) - np.diag(cm)
     TP = np.diag(cm)
     TN = cm.sum() - (FP + FN + TP)
-
+    print('\n',"#"*20,' Measurments using {} features in {}'.format(k, classifier_)," ","#"*20,'\n')
+    print('{} Measurement table:'.format(classifier_))
     print(FP)
     print(FN)
     print(TP)
@@ -162,22 +190,24 @@ def other_measures(cm, k):
     # Overall accuracy
     ACC = (TP+TN)/(TP+FP+FN+TN)
 
-    print('\n\n',"#"*20,' Measurments for {} features '.format(k)," ","#"*20,'\n')
 
-    print("PPV:{}\nNPV:{}\nSensitivity:{}\nSpecificity:{}".format(PPV, NPV, TPR, TNR))
+    print("{} ===> PPV:{}\nNPV:{}\nSensitivity:{}\nSpecificity:{}".format(classifier_, PPV, NPV, TPR, TNR))
     print('\n')
-    print("Accuracy:",ACC)
+    print("{} ===> Accuracy:".format(classifier_, ACC))
 
     avg_PPV  = np.average(PPV, axis=None, weights=None, returned=False)
     avg_NPV  = np.average(NPV, axis=None, weights=None, returned=False)
     avg_TPR  = np.average(TPR, axis=None, weights=None, returned=False)
     avg_TNR  = np.average(TNR, axis=None, weights=None, returned=False)
     avg_ACC  = np.average(ACC, axis=None, weights=None, returned=False)
-    print("PPV:{:.2f}\nNPV:{:.2f}\nSensitivity:{:.2f}\nSpecificity:{:.2f}\nAccuracy:{:.2f}".format(avg_PPV, avg_NPV, avg_TPR, avg_TNR, avg_ACC))
-    print('\n\n\n')
+    print("{} ===> PPV:{:.2f}\nNPV:{:.2f}\nSensitivity:{:.2f}\nSpecificity:{:.2f}\nAccuracy:{:.2f}".format(classifier_, avg_PPV, avg_NPV, avg_TPR, avg_TNR, avg_ACC))
+    print('\n')
 
 for _, k in enumerate(number_of_features_as_K):
-    other_measures(cm[k], k)
+    other_measures(cm[k], k, 'SVM')
+    other_measures(cm_RF[k], k, 'RF')
+    other_measures(cm_DT[k], k, 'DT')
+    
 ################################################################
 #%%
 #
